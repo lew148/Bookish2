a14ed7f8fd746bb709bfa81bc0831774bf2ac5a4
Taiwo <Taiwo.Olateju@softwire.com>
2018-09-24 12:16:39 +0100

    Move JDB(I/C) methods to GeneralService class

--- src/main/java/org/softwire/training/bookish/services/GeneralService.java
+++ src/main/java/org/softwire/training/bookish/services/GeneralService.java
@@ -1,7 +1,9 @@
 package org.softwire.training.bookish.services;
 
 import org.jdbi.v3.core.Jdbi;
+import org.softwire.training.bookish.databaseModels.Book;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +16,39 @@ public class GeneralService {
     private String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
     private Jdbi jdbi = Jdbi.create(connectionString);
+
+    public void jdbcMethod() throws SQLException {
+        Connection connection = DriverManager.getConnection(connectionString);
+        try (Statement statement = connection.createStatement()) {
+
+            String query = "SELECT * FROM books";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                int bookId = resultSet.getInt("id");
+                String author = resultSet.getString("author");
+                String title = resultSet.getString("title");
+
+                System.out.println("Book ID: " + bookId + " has author: '" + author + "' and title: '" + title + "'");
+            }
+        }
+    }
+
+    public void jdbiMethod() {
+        Jdbi jdbi = Jdbi.create(connectionString);
+
+        List<Book> books = jdbi.withHandle(handle ->
+                handle.createQuery("SELECT * FROM books")
+                        .mapToBean(Book.class)
+                        .list()
+        );
+
+        for (Book book: books) {
+            System.out.println("Book ID: " + book.getIdBooks() + "' and title: '" + book.getBookName() + "'");
+        }
+
+
+    }
 
     public ArrayList getAll(Class x, String tableName){
         Jdbi jdbi = Jdbi.create(connectionString);
